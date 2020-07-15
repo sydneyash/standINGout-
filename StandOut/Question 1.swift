c0bd17ca8c3ad51ed236e529dc69299e6c479470
Sydney Ash <sydneyash2002@yahoo.com>
2020-07-15 11:43:21 -0400

    i feel anger

---
+++ StandOut/Question 1.swift
@@ -0,0 +1,30 @@
+//
+//  Question 1.swift
+//  StandOut
+//
+//  Created by Sydney Ash on 7/14/20.
+//  Copyright © 2020 Erin Salva. All rights reserved.
+//
+
+import UIKit
+
+class Question_1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
